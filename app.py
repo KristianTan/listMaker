ceab000891f0e02ecedca73b7b269fc527c1a422
Kristian Tan <kristian.tan@ntlworld.com>
2020-03-25 19:20:20 +0000

    ADded temporary route to get new list

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/sendData', methods=['GET', 'POST'])
+def sendData():
+    title = request.form['title']
+    passphrase = request.form['passphrase']
+    output = title + ", " + passphrase
+    return jsonify({'output': output})
 
 
 if __name__ == '__main__':
